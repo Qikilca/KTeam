a42ab4ff18b0779f49b840fc34e55e576bdb172c
Qikilca <mizu20000618@icloud.com>
2021-12-10 21:36:51 +0900

    index.js更新

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <Button variant="contained">Hello Next</Button>
   }
   
   export default HomePage
