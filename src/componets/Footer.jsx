1af884cc5f3e4c18909990cb0a2bb7e88379197c
Qikilca <Qikilca8190@users.noreply.github.com>
2021-12-15 23:51:33 +0900

    Footerのjsxをコンポーネントとして作成

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+
+    return(
+        <footer>
+            <div>
+
+                <p>
+                    This is Footer
+                </p>
+
+            </div>
+            
+        </footer>
+    )
+    
+}
+
+export default Footer;
