d690d603aa0fbc5ee101f7ac716b0135f6a95b8a
Qikilca <Qikilca8190@users.noreply.github.com>
2021-12-15 23:54:00 +0900

    Footerをコンポーネントとして呼び込むように修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,31 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+import {Button, TextField} from '@mui/material';
+import Footer from '../componets/Footer';
 
 
 function HomePage() {
-    return <Button variant="contained">Hello Next</Button>
-  }
-  
-  export default HomePage
+
+    return(
+        
+        <div>
+            
+            
+
+            <h1>
+                Welcom to Next.js
+            </h1>
+
+            <Button variant="contained">
+                Add Button
+            </Button>
+
+            <Footer></Footer>
+        </div>
+
+    )
+
+
+}
+
+export default HomePage;
