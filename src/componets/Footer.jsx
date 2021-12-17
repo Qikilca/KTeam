5558860769ffcc4c542344c83da92477f9c33558
Qikilca <Qikilca8190@users.noreply.github.com>
2021-12-17 16:42:52 +0900

    フッターのjsx作成

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,18 +1,19 @@
+import { Grid } from "@mui/material";
+
+
 function Footer() {
 
-    return(
+    return (
         <footer>
-            <div>
-
-                <p>
-                    This is Footer
-                </p>
-
-            </div>
+            <Grid container>
+                <Grid item xs={3}>3</Grid>
+                <Grid item xs={6}>6</Grid>
+                <Grid item xs={3}>3</Grid>
+            </Grid>
             
         </footer>
     )
-    
+
 }
 
 export default Footer;
