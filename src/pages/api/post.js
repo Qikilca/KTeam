4a250b012fc81745dd7d05582f1bb761f76044cb
Qikilca <Qikilca8190@users.noreply.github.com>
2022-01-24 02:40:17 +0900

    DBを読み込むためのapiを追加

--- src/pages/api/post.js
+++ src/pages/api/post.js
@@ -1,19 +1,25 @@
-import { ORDER } from "mysql/lib/PoolSelector";
 import { sql_query } from "../../../lib/db";
+
 
 const handler = async (_, res) => {
     try {
         const results = await sql_query(`
-        SELECT * FROM test
-        ORDER BY id DESC
-        LIMIT 10
+        SELECT * FROM user
         `);
 
         return res.json(results);
-    }catch(e){
-        res.status(500).json({message:e.message});
+    } catch (e) {
+        res.status(500).json({ message: e.message });
     }
 
 };
 
 export default handler;
+/*
+export default async (name) => {
+    const result = await db.query(`
+    SELECT * FROM user;
+    `)
+    return result
+}
+*/
